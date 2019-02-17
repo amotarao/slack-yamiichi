f806409d37183d4a9bf7f1d43469e2ed879d1bf5
Amon Sawamura <amon.sawamura@gmail.com>
2019-02-17 16:06:36 +0900

    feature(date): add example module

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from 'express';
+import * as moment from 'moment-timezone';
+
+moment.tz.setDefault('Asia/Tokyo');
 
 export const example = (req: Request, res: Response): void => {
-  const message = req.query.message || req.body.message || 'Hello World!';
+  const date = moment().toLocaleString();
+  const message =
+    req.query.message || req.body.message || `Hello World! ${date}`;
   res.status(200).send(message);
 };
