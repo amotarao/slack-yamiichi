c3c1a221240fd82d5ebf8491bd194f1f6f42b2f8
Amon Sawamura <amon.sawamura@gmail.com>
2019-02-17 19:58:19 +0900

    feature(enum): names, ids

---
+++ src/common/enums.ts
@@ -0,0 +1,24 @@
+export enum Names {
+  ExhibitionFormTitle = '闇市出品フォーム',
+  Exhibit = '出品',
+}
+
+export enum IDs {
+  Create = 'create',
+}
+
+export enum ExhibitElementNames {
+  Name = '商品名',
+  Description = '商品説明',
+  StartValue = '開始価格',
+  EndValue = '即決価格',
+  ExhibitionPeriod = '出品期間',
+}
+
+export enum ExhibitElementIDs {
+  Name = 'name',
+  Description = 'description',
+  StartValue = 'start_value',
+  EndValue = 'end_value',
+  ExhibitionPeriod = 'exhibition_period',
+}
