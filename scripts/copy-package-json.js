68aeb173c3fb0a266758131f757558cdbc3f82dc
Amon Sawamura <amon.sawamura@gmail.com>
2019-02-17 19:57:35 +0900

    feature(copy-package-json): recursive path

--- scripts/copy-package-json.js
+++ scripts/copy-package-json.js
@@ -1,15 +1,27 @@
 const fs = require('fs');
 const base = require('../package.json');
+const { dir } = require('yargs').argv;
 
 const { name, version, main, dependencies } = base;
 const data = { name, version, main, dependencies };
 const json = JSON.stringify(data, null, 2);
 
-if (!fs.existsSync('./build')) {
-  fs.mkdirSync('./build');
+function mkdir(pathArray, base = '.') {
+  const [dir, ...children] = pathArray;
+  const newDir = `${base}/${dir}`;
+
+  if (!fs.existsSync(newDir)) {
+    fs.mkdirSync(newDir);
+  }
+
+  if (children.length) {
+    mkdir(children, newDir);
+  }
 }
 
-fs.writeFile('./build/package.json', json, err => {
+mkdir(dir.split('/'));
+
+fs.writeFile(`${dir}/package.json`, json, err => {
   if (err) {
     throw err;
   }
