18f25b5a5d56e963d91842a98634b2691e5940ee
Amon Sawamura <amon.sawamura@gmail.com>
2019-02-17 15:44:41 +0900

    lint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { Request, Response } from 'express'
+import { Request, Response } from 'express';
 
-export const example = (req: Request, res: Response) => {
+export const example = (req: Request, res: Response): void => {
   const message = req.query.message || req.body.message || 'Hello World!';
   res.status(200).send(message);
 };
